23a0dd921205aad840ba4ae6cc3cd850a985cfe3
zhenlei <839238345@qq.com>
2017-08-04 16:58:44 +0800

    新增上下文类支持事物

--- EInfrastructure/Infrastructure/Interfaces/Repository/IUnitOfWork.cs
+++ EInfrastructure/Infrastructure/Interfaces/Repository/IUnitOfWork.cs
@@ -5,40 +5,54 @@ namespace EInfrastructure.Infrastructure.Interfaces.Repository
 {
     public interface IUnitOfWork : IDisposable
     {
-        #region Interface
 
-        #region Commit
         /// <summary>
-        /// Commit all changes made in  a container.
+        /// 提交更改
         /// </summary>
-        ///<remarks>
-        /// If entity have fixed properties and optimistic concurrency problem exists 
-        /// exception is thrown
-        ///</remarks>
+        /// <returns></returns>
         int Commit();
-        #endregion
 
+        /// <summary>
+        /// 异步提交
+        /// </summary>
+        /// <returns></returns>
         Task<int> CommitAsync();
 
-        #region CommitAndRefreshChanges
         /// <summary>
-        /// Commit all changes made in  a container.
+        /// 提交并刷新更改
         /// </summary>
-        ///<remarks>
-        /// If entity have fixed properties and optimistic concurrency problem exists 
-        /// client changes are refereshed
-        ///</remarks>
         void CommitAndRefreshChanges();
-        #endregion
 
-        #region RollbackChanges
         /// <summary>
-        /// Rollback changes not stored in databse at 
-        /// this moment. See references of UnitOfWork pattern
+        /// 回滚
         /// </summary>
         void RollbackChanges();
-        #endregion
 
-        #endregion
+        /// <summary>
+        /// 使用事物，为保存存储过程与上下文一致（后续需要存储过程的返回值）
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="func">委托（得到存储过程返回的值）</param>
+        /// <param name="action">委托（执行剩余的业务）</param>
+        /// <param name="funResult">存储过程返回值信息</param>
+        /// <returns></returns>
+        int UseTransaction<T>(Func<T> func, Action<T> action, out T funResult);
+
+        /// <summary>
+        /// 使用事物，为保存存储过程与上下文一致（后续需要存储过程的返回值）
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="func">委托（得到存储过程返回的值）</param>
+        /// <param name="action">委托（执行剩余的业务）</param>
+        /// <returns></returns>
+        int UseTransaction<T>(Func<T> func, Action<T> action);
+
+        /// <summary>
+        /// 使用事物，为保存存储过程与上下文一致（两个委托无任务关联）
+        /// </summary>
+        /// <param name="action1">委托1，执行事物的委托</param>
+        /// <param name="action2">委托2，一般委托</param>
+        /// <returns></returns>
+        int UseTransaction(Action action1, Action action2);
     }
 }
